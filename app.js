c6320084dfbc02b23c8c1186e3ad361610939b51
sebb22 <cocowarrh@gmail.com>
2020-03-02 11:08:09 +0100

    working on js to set datas onto li to display event coordinate

--- app.js
+++ app.js
@@ -1,74 +1,76 @@
 var app = {
 
-    buttonElement : document.querySelector("#surprise"),
+  buttonElement: document.querySelector("#surprise"),
 
-    ulElement:document.createElement("ul"),
+  ulElement: document.createElement("ul"),
 
-   init: function () {
-app.buttonElement.addEventListener("click",app.handleAjaxCall);
+  init: function () {
+    app.buttonElement.addEventListener("click", app.handleAjaxCall);
 
-   },
+  },
 
-   handleAjaxCall: function(){
-      event.preventDefault();
-      if(app.ulElement.hasChildNodes()){
-console.log("true");
-app.ulElement="";
-}
+  handleAjaxCall: function () {
+    event.preventDefault();
+    if (app.ulElement.hasChildNodes()) {
+      console.log("true");
+      app.ulElement = "";
+    }
 
-      console.log('click!');
-      app.ajaxGet("https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?limit=1",app.callback);
-      document.querySelector("#test").appendChild(app.ulElement);
-   },
+    console.log('click!');
+    app.ajaxGet("https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?limit=4", app.callback);
+    document.querySelector("#test").appendChild(app.ulElement);
+  },
 
-   ajaxGet: function(url,callback){
-     var req = new XMLHttpRequest();
-   req.open("GET", url);
-   req.addEventListener("load", function () {
-       if (req.status >= 200 && req.status < 400) {
-           // Appelle la fonction callback en lui passant la réponse de la requête
-           app.callback(req.responseText);
-       } else {
-           console.error(req.status + " " + req.statusText + " " + url);
-       }
-   });
-   req.addEventListener("error", function () {
-       console.error("Erreur réseau avec l'URL " + url);
-   });
-   req.send(null);
-   },
+  ajaxGet: function (url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+      if (req.status >= 200 && req.status < 400) {
+        // Appelle la fonction callback en lui passant la réponse de la requête
+        app.callback(req.responseText);
+      } else {
+        console.error(req.status + " " + req.statusText + " " + url);
+      }
+    });
+    req.addEventListener("error", function () {
+      console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+  },
 
-   callback:function(response){
-     var response = JSON.parse(response);
-     console.log(response);
-//console.log(response["events"][i]["geometries"]["coordinate"]);
-     for(var i=0;i<=response.events.length;i++){
-         var eventItem=response["events"][i]["title"];
-         console.log(response["events"][i]["geometries"]["coordinates"]);
-//console.log(eventCoordinates);
-app.displayResponseItems(eventItem,eventCoordinates);
-     }
- 
-   },
+  callback: function (response) {
+    var response = JSON.parse(response);
+    //about setting apart events
+    response = response.events;
+    // console.log(response);
 
-   displayResponseItems:function(eventItem,eventCoordinates){
-  if(eventItem!=""){
- 
-   var itemElement = document.createElement("li");
-   itemElement.innerHTML=eventItem;
-   //itemElement.dataset=eventItem;
-  
-   app.ulElement.appendChild(itemElement);
-itemElement.addEventListener("click",app.handleDisplayMap);
+    //loop displaying every item
+    for (var i = 0; i <= response.length; i++) {
+      var eventItem = response[i];
+      // console.log(eventItem.title);
+      // console.log(eventItem.geometries);
+      app.displayResponseItems(eventItem);
+    }
+  },
+
+  displayResponseItems: function (eventItem) {
+    if (eventItem != "") {
+
+      var itemElement = document.createElement("li");
+      itemElement.innerHTML = eventItem.title;
+      //itemElement.dataset=eventItem;
+
+      app.ulElement.appendChild(itemElement);
+      itemElement.addEventListener("click", app.handleDisplayMap);
+    }
+  },
+
+  handleDisplayMap: function () {
+    console.log("pop");
+    var itemElement = event.currentTarget;
+    //console.log(itemElement.data);
+
   }
-},
-
-handleDisplayMap:function(){
- console.log("pop");
- var itemElement=event.currentTarget;
- //console.log(itemElement.data);
- 
-}
 
 }
 
